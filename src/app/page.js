860c692314af101d4cead1b2b5090bbd20ae101b
Nitin Kumar <112533120+nitinkumar9877@users.noreply.github.com>
2025-08-21 17:00:18 +0530

    first

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Col, Container, Row } from "react-bootstrap";
+import "../../public/sass/pages/homepage.scss";
+export default function Home() {
+  return (
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+}
