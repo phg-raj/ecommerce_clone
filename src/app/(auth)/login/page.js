969f30a2c29d944d453a6ff40a689ec71bd5cba0
phg-raj <raj.saab.2002@gmail.com>
2025-08-23 15:05:35 +0530

    header

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -24,12 +24,58 @@ const Header = () => {
   return (
 
     <section className="header">
+      {/* 
+      <Container>
+        <Row>
+          <Col xs={12}>
+            <div className="inner_div_header">
+
+              <div className="left_area">
+                <div className="bar_logo">
+                  <FontAwesomeIcon icon={["fas", "bars"]} className="bar_icon" />
+                  <div className="logo_text">meesho</div>
+                </div>
+
+
+                <div className="search_bar">
+                  <FontAwesomeIcon icon={["fas", "search"]} className="search_icon" />
+                  <input
+                    type="text"
+                    placeholder="Try Saree, Kurti or Search by"
+                  />
+                </div>
+              </div>
+              <div className="right_area">
+                <div className="utility_links">
+                  <div className="vertical-divider"></div>
+
+                  <Link href="#" className="icon">
+                    <FontAwesomeIcon icon={["fas", "user"]} className="profile" />
+                    Profile
+                  </Link>
+                  <Link href="#" className="icon">
+                    <FontAwesomeIcon icon={["fas", "cart-plus"]} className="cart" />
+                    cart
+                  </Link>
+                </div>
+              </div>
+
+            </div>
+          </Col>
+        </Row>
+      </Container> */}
+
+
+
       <Container>
         <Row>
           <Col xxl={12} xl={12} lg={12} md={12} xm={12} xs={12}>
             <div className="inner_div_header">
 
+              
+
               <div className="logo_text">meesho</div>
+
 
 
               <div className="search_bar">
@@ -41,89 +87,89 @@ const Header = () => {
               </div>
 
 
-              <div className="utility_links">
 
-                <div className="vertical-divider"></div>
-                <Link href="#" className="icon">
-                  <span className="profile_icon">&#128100;</span>
-                  Profile
-                </Link>
-                <Link href="#" className="icon">
-                  <span className="cart_icon">&#128722;</span>
-                  Cart
-                </Link>
+
+              <div className="utility_links">
+                <div className="links_wrapper">
+                  <Link href="#" className="icon">
+                    <span className="profile_icon">&#128100;</span>
+                    Profile
+                  </Link>
+                  <Link href="#" className="icon">
+                    <span className="cart_icon">&#128722;</span>
+                    Cart
+                  </Link>
+                </div>
+
+                <div className="bar_logo">
+                  <FontAwesomeIcon icon={["fas", "bars"]} className="bar_icon" />
+
+                </div>
               </div>
             </div>
           </Col>
         </Row>
-      </Container>
+      </Container>                                                                                                                                                                            
+
       <Container>
         <Row>
           <Col xxl={12} xl={12} lg={12} md={12} xm={12} xs={12}>
             <div className="inner_div_bottom">
-              <nav >
-
+              <nav>
                 <div className="img_bottom">
 
-                <div className="logo_area" >
-                  <Image src={Mobiles} alt=".." title=".." fetchPriority='high' width={48} height={48} />
-                </div>
-                <div className="text_area">
-                  <ul><li><Link href="#" className="icon">Mobiles & Tablets </Link> </li>
-                  </ul>
-                </div>
+                  {/* <div className="logo_area">
+              <Image src={Minutes} alt="Minutes" width={48} height={48} />
+              <div className="text_area"><ul><li><Link href="#">Minutes</Link></li></ul></div>
+            </div> */}
 
+                  <div className="logo_area">
+                    <Image src={Mobiles} alt="Mobiles" width={48} height={48} />
+                    <div className="text_area"><ul><li><Link href="#">Mobiles & Tablets</Link></li></ul></div>
+                  </div>
 
-                <div className="logo_area" >
-                  <Image src={Fashion} alt=".." title=".." fetchPriority='high' width={48} height={48} />
-                </div>
-                <div className="text_area"><ul><li><Link href="#" className="icon">Fashion </Link> </li>
-                </ul>
-                </div>
+                  <div className="logo_area">
+                    <Image src={Fashion} alt="Fashion" width={48} height={48} />
+                    <div className="text_area"><ul><li><Link href="#">Fashion</Link></li></ul></div>
+                  </div>
 
+                  <div className="logo_area">
+                    <Image src={Electronics} alt="Electronics" width={48} height={48} />
+                    <div className="text_area"><ul><li><Link href="#">Electronics</Link></li></ul></div>
+                  </div>
 
-                <div className="logo_area" >
-                  <Image src={Electronics} alt=".." title=".." fetchPriority='high' width={48} height={48} />
-                </div>
-                <div className="text_area">
-                  <ul><li><Link href="#" className="icon">Electronics</Link> </li>
-                  </ul>
+                  <div className="logo_area">
+                    <Image src={Home} alt="Home & Furniture" width={48} height={48} />
+                    <div className="text_area"><ul><li><Link href="#">Home & Furniture</Link></li></ul></div>
+                  </div>
 
-                </div>
+                  <div className="logo_area">
+                    <Image src={TVs} alt="TVs & Appliances" width={48} height={48} />
+                    <div className="text_area"><ul><li><Link href="#">TVs & Appliances</Link></li></ul></div>
+                  </div>
+                  {/* 
+            <div className="logo_area">
+              <Image src={Flights} alt="Flight Bookings" width={48} height={48} />
+              <div className="text_area"><ul><li><Link href="#">Flight Bookings</Link></li></ul></div>
+            </div> */}
 
-                <div className="logo_area" >
-                  <Image src={TVs} alt=".." title=".." fetchPriority='high' width={48} height={48} />
-                </div>
-                <div className="text_area">
-                  <ul><li><Link href="#" className="icon">TVs & Appliances</Link> </li>
-                  </ul>
-                </div>
-
-
-                <div className="logo_area" >
-                  <Image src={Home} alt=".." title=".." fetchPriority='high' width={48} height={48} />
-                </div>
-                <div className="text_area">
-                  <ul><li><Link href="#" className="icon">Beauty, Food</Link> </li>
-                  </ul>
-                </div>
-
-
-                <div className="logo_area" >
-                  <Image src={Beauty} alt=".." title=".." fetchPriority='high' width={48} height={48} />
-                </div>
-                <div className="text_area">
-                  <ul><li><Link href="#" className="icon">Home & Furniture</Link> </li>
-                  </ul>
-                </div>
+                  <div className="logo_area">
+                    <Image src={Beauty} alt="Beauty & Food" width={48} height={48} />
+                    <div className="text_area"><ul><li><Link href="#">Beauty, Food...</Link></li></ul></div>
+                  </div>
+                  {/* 
+            <div className="logo_area">
+              <Image src={Grocery} alt="Grocery" width={48} height={48} />
+              <div className="text_area"><ul><li><Link href="#">Grocery</Link></li></ul></div>
+            </div> */}
 
                 </div>
-
               </nav>
             </div>
           </Col>
         </Row>
       </Container>
+
     </section>
 
 
